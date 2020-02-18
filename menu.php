594cb06cf67fd6ace8caa3d3df04f710639d8500
jenny80170 <jennifer.malleret@outlook.fr>
2020-02-18 11:33:07 +0100

    ajout du responsive

--- menu.php
+++ menu.php
@@ -1,13 +1,14 @@
 <div id="menuMobil" class="row">
-  <div class="container col-3">
+  <div class="container col-md-3">
     <!--Card avec les coordonnées du client-->
-    <div class="card bg-primary text-dark text-center font-weight-bold d-inline-block mt-5 ml-3">
+    <div class="card coordinatedPhone bg-primary text-dark text-center font-weight-bold d-inline-block mt-5 ml-3">
       <div class="card-header font-weight-bold">Coordonnées</div>
       <div class="card-body">
         <p class="card-text text-dark font-weight-bold">
           Nom : Foucart<br>
           Prénom : Florian<br>
           <i class="fa fa-phone" aria-hidden="true"> : 06 34 30 63 43</i><br><br>
+          <div class="displayNonePhone">
           <!--Bouton de contact qui au click renvoie sur un formulaire de contact-->
           <button type="button" class="btn btn-secondary font-weight-bold" data-toggle="modal" id="buttonContact" onclick="window.location.href = 'contact.php';">Contactez-moi</button>
           <!--Bouton de contact qui au click renvoie sur un calendrier-->
@@ -16,6 +17,7 @@
         </p>
       </div>
       <!--Fin de card coordonnées client-->
+      <div class="displayNonePhone">
       <div class="d-flex justify-content-center">
         <p>
           <!--Bouton icon modal géolocalisation-->
@@ -24,6 +26,7 @@
           </button>
         </p>
       </div>
+    </div>
       <!-- Début de la modal géolocalisation -->
       <div class="modal" id="exampleModalCentered" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -34,7 +37,7 @@
           </div>
           <div class="modal-body">
             <!--Insertion de la carte maps-->
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49027.25880909703!2d2.4491935188965797!3d49.7733158396207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e793a6198fef93%3A0xe0263c33339324c7!2s80110%20Moreuil!5e0!3m2!1sfr!2sfr!4v1574153605631!5m2!1sfr!2sfr" width="600" height="450" allowfullscreen=""></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49027.25880909703!2d2.4491935188965797!3d49.7733158396207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e793a6198fef93%3A0xe0263c33339324c7!2s80110%20Moreuil!5e0!3m2!1sfr!2sfr!4v1574153605631!5m2!1sfr!2sfr" width="600" height="450" allowfullscreen="" class="img-fluid"></iframe>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary justify-content-center" data-dismiss="modal">Fermer</button>
@@ -43,6 +46,7 @@
       </div>
       <!--Fin de la modal géolocalisation-->
       <!--Card avis utilisateur-->
+      <div class="displayNonePhone">
       <div class="card text-dark text-center font-weight-bold bg-primary mb-3 d-inline-block">
         <div class="card-header">Avis</div>
         <div class="card-body">
@@ -57,5 +61,7 @@
           </div>
         </div>
       </div>
+      </div>
     </div>
+  </div>
     <!--Fin de la card avis utilisateur-->
